c69f9d1bd891305bb3faea827732a0efe7685e54
HanadaMasora <hanadamasora@example.com>
2026-01-05 19:29:32 +0900

    初回コンテンツ追加

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+// src/content/config.ts
+import { defineCollection, z } from "astro:content";
+
+export const collections = {
+  novels: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z.string(),
+      type: z.string(),
+      series: z.string().optional(),
+      published: z.string().optional(),
+    }),
+  }),
+};
