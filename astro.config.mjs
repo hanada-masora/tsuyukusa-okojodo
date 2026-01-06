09964e8befc83b5911919152b49407b3ce655da2
HanadaMasora <hanadamasora@example.com>
2026-01-06 18:17:08 +0900

    OGPついかしてみた

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+const site =
+  process.env.NODE_ENV === "production"
+    ? "https://tsuyukusa-okojodo.pages.dev" // ← 本番URL
+    : "http://localhost:4321";      // ← ローカル
+
 export default defineConfig({
+  site,
+
   vite: {
     resolve: {
       alias: {
